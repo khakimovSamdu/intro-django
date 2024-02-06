6e5135cc0919271f5dd1ea7303d1ec62268db54a
khakimovSamdu <allamurodxakimov@gmail.com>
2024-02-06 18:44:15 +0500

    intro-django

---
+++ intro/urls.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.urls import path
+import json
+
+def index(request: HttpRequest):
+    a = int(request.GET.get('a','0'))
+    b = int(request.GET.get('b','0'))
+    data = {
+        "a":a,
+        "b":b
+    }
+    return JsonResponse(data)
+def respons_index(request: HttpRequest):
+    if request.method == 'POST':
+        data = request.POST
+        body = request.body
+        # Convert bytes to string
+        body = body.decode('utf-8')
+        # Convert string to dictionary
+        data = json.loads(body)
+        
+    return HttpResponse(data['a']+data['b'])
+
+urlpatterns = [
+    path('',respons_index)
+]
