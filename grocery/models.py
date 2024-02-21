335f6c2bf6a2907a625c2d186c80776b50fe3a10
khakimovSamdu <allamurodxakimov@gmail.com>
2024-02-21 08:33:25 +0500

    typeing field

--- grocery/models.py
+++ grocery/models.py
@@ -5,9 +5,11 @@ class Backend(models.Model):
     name = models.TextField()
     price = models.TextField()
     quantity = models.TextField()
+    char_text = models.CharField(max_length = 8, blank = True)
 
     def __str__(self) -> str:
         return self.name +"  " + self.price + "  " + self.quantity
+
 class Frontend(models.Model):
     name = models.TextField()
     price = models.TextField()
